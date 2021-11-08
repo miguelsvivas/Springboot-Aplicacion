08151e8381b5c8073f08915b4a47b4126110eca3
Miguel <vivasmiguel96@gmail.com>
2021-11-07 19:38:12 -0500

    Password

---
+++ src/main/java/com/aplicacion/service/UserDetailsServiceImpl.java
@@ -0,0 +1,46 @@
+package com.aplicacion.service;
+
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aplicacion.entity.Role;
+import com.aplicacion.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+
+			com.aplicacion.entity.User appUser = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Login Username invalido"));
+			
+			Set<GrantedAuthority> grantList = new HashSet<GrantedAuthority>(); 
+			for (Role role: appUser.getRoles()) {
+				GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getDescription());
+	            grantList.add(grantedAuthority);
+			}
+		
+		
+			UserDetails user = (UserDetails) new User(username,appUser.getPassword(),grantList);
+
+			return user;
+	}
+
+}
